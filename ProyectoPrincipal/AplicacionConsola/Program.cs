9ac94d7468b1d66e366a901f4c2bb8ae92f9716f
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2025-10-20 02:48:04 +0200

    .

---
+++ ProyectoPrincipal/AplicacionConsola/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Libreria1;
+
+Console.WriteLine("Hello, World!");
+
+
+var prueba = new PruebaLibreria1();
+
+Console.WriteLine(prueba.Imprimir());
